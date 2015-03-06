453e001e4f2967a2c2263d5d39e8bbd9dfe1a0ad
doaaanwar <doaa.anwar@gmail.com>
2015-03-05 20:33:48 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -142,7 +142,7 @@ $(document).ready(function(){
 
     function populateDB(tx) {
         $('#busy').show();
-        //tx.executeSql('DROP TABLE IF EXISTS item');
+        tx.executeSql('DROP TABLE IF EXISTS item');
         var sql = 
             "CREATE TABLE IF NOT EXISTS item ( "+
             "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -152,9 +152,9 @@ $(document).ready(function(){
             "totalWeight FLOAT, " +
             "picture VARCHAR(200))";
         tx.executeSql(sql);
-        //sql = "INSERT INTO item (id,itemName,weight,totalWeight,numOfPackage,picture) VALUES (0,'test',200,0,0,'null')";
+        sql = "INSERT INTO item (id,itemName,weight,totalWeight,numOfPackage,picture) VALUES (0,'test',200,0,0,'null')";
 
-        //tx.executeSql(sql);
+        tx.executeSql(sql);
     }
 
 
